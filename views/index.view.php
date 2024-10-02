394624c8431d0391b0a2ebbe27cfc48f1c7a1a76
Jay Beitsma <s1214179@student.windesheim.nl>
2024-10-02 13:30:54 +0200

    Kleine aanpassingen aan header, en begin intro pagina

--- views/index.view.php
+++ views/index.view.php
@@ -5,7 +5,15 @@ require 'layout/header.php';
 ?>
 
 <main>
-    index
+    <style>
+        @import "../views/css/index.css";
+    </style>
+    <div class="info">
+        <h1>Hallo,</h1>
+    </div>
+    <div class="imageHolder">
+        <img src="../views/img/RETROWAVE-OASIS-33331.png" alt="test">
+    </div>
 </main>
 
 <?php
