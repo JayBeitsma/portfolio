44ee3d7ef86f0a62edd7311281df3f30688a6d20
Jay Beitsma <s1214179@student.windesheim.nl>
2024-10-10 12:04:46 +0200

    Created form on contact page thats saves data to database, changed all coding language to english to keep everything the same

--- views/contact.view.php
+++ views/contact.view.php
@@ -5,7 +5,17 @@ require 'layout/header.php';
 ?>
 
 <main>
-
+    <form id="message" name="message" method="post">
+        <label>Naam:</label>
+        <input type="text" name="name" minlength="3" maxlength="64" required>
+        <label>Email:</label>
+        <input type="email" name="email" required>
+        <label>Telefoon nummer:</label>
+        <input type="tel" name="phone" maxlength="20" required>
+        <label>Bericht:</label>
+        <input type="text" name="message" required>
+        <button type="submit">Verstuur bericht</button>
+    </form>
 </main>
 
 <?php
